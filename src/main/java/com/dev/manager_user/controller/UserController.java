cccb9577cb9f7b66c0ce2e634aa609a6a911721c
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-05 01:12:58 +0100

    Using the user DTO in controller

--- src/main/java/com/dev/manager_user/controller/UserController.java
+++ src/main/java/com/dev/manager_user/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.dev.manager_user.controller;
 
+import com.dev.manager_user.dto.UserDTO;
 import com.dev.manager_user.model.User;
-import com.dev.manager_user.repository.UserRepository;
 import com.dev.manager_user.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,17 @@ public class UserController {
 
     // POST /users -> Create a new User
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user){
+    public ResponseEntity<User> create(@RequestBody @Valid UserDTO userDTO){
         try {
+            User user = new User();
+            user.setName(userDTO.getName());
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+
             User newUser = userService.createUser(user);
             return ResponseEntity.ok(newUser);
         } catch (RuntimeException e){
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(null);
         }
     }
 
